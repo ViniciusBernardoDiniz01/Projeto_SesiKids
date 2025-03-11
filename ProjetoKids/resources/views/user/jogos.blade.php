78f8b57b9c7ed64ee430da99c84868706fa54ff3
Vinicius <vinicius.diniz.senai404@gmail.com>
2025-03-11 08:30:11 -0300

    update

--- ProjetoKids/resources/views/user/jogos.blade.php
+++ ProjetoKids/resources/views/user/jogos.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ URL:: asset('../../../public/CSS/jogos.css')}}">
     <title>Escolha um jogo</title>
     <style>
         *{
